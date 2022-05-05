39d1f605ed486fdfcce3f0c6ecb7105017d1258d
Haydar Mehryar <haydarmehryar@gmail.com>
2022-05-05 15:21:26 -0400

    resolves #24 it shows the result of 1k matter

---
+++ jetscape_ml_tensorflow_nn_dataset_builder_file_concatenator.sh
@@ -0,0 +1,5 @@
+#Converting jupyter notebook to python script
+echo "Converting notebook to script"
+jupyter nbconvert --to python jetscape_ml_tensorflow_nn_dataset_builder_file_concatenator.ipynb
+
+python jetscape_ml_tensorflow_nn_dataset_builder_file_concatenator.py
