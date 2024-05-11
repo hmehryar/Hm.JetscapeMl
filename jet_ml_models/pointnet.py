93e82037cec19f750f44ff4bedfd654322500e12
Haydar Mehryar <haydarmehryar@gmail.com>
2024-05-11 17:01:13 -0400

    working pointnet_alpha_s_2_epoch_no_folding resolves #309 @hmehryar

--- jet_ml_models/pointnet.py
+++ jet_ml_models/pointnet.py
@@ -1,9 +1,16 @@
+import json
+from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.callbacks import ModelCheckpoint
+from time import time
+
 
 def conv_bn(x, filters):
     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
@@ -214,6 +221,143 @@ def preprocess_dataset(dataset_x, dataset_y):
   print("y_test_categorical_encoded:\n",y_test_categorical_encoded[:10])
 
   return (x_train_points,  y_train_categorical_encoded,x_test_points,  y_test_categorical_encoded)
+
+
+
+
+def train_model_with_callbacks(model, x_train, y_train, x_test, y_test, monitor="val_accuracy", best_model_file_path="best_model.h5", n_epochs=10):
+    """
+    Train a TensorFlow model with specified callbacks for checkpointing and early stopping.
+
+    Parameters:
+    - model (tf.keras.Model): The TensorFlow model to be trained.
+    - x_train (numpy.ndarray): The training data features.
+    - y_train (numpy.ndarray): The training data labels.
+    - x_test (numpy.ndarray): The test data features.
+    - y_test (numpy.ndarray): The test data labels.
+    - monitor (str): Quantity to be monitored for early stopping and checkpointing. Default is "val_accuracy".
+    - best_model_file_path (str): File path to save the best model checkpoint. Default is "best_model.h5".
+    - n_epochs (int): Number of epochs for training. Default is 10.
+
+    Returns:
+    - model (tf.keras.Model): The trained TensorFlow model.
+    - history (tf.keras.callbacks.History): A History object containing training/validation metrics.
+    - train_time (float): Time taken for training in minutes.
+    """
+
+    # Define callbacks
+    checkpoint_callback = ModelCheckpoint(
+        best_model_file_path,
+        monitor=monitor,
+        save_best_only=True,
+        mode="max" if monitor == "val_accuracy" else "min",
+        verbose=1
+    )
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor=monitor,    # Quantity to be monitored
+        patience=3,         # Number of epochs with no improvement after which training will be stopped
+        verbose=1           # Verbosity mode. 1: print messages when triggered, 0: silent
+    )
+    callbacks = [checkpoint_callback, early_stopping_callback]
+
+    start = time()
+    # Train the model
+    history = model.fit(
+        x_train,
+        y_train,
+        epochs=n_epochs,
+        validation_data=(x_test, y_test),
+        callbacks=callbacks
+    )
+    train_time = (time()-start)/60.0
+    return model, history, train_time
+
+def save_training_history(history,simulation_path):
+  # Save the training history to a file (e.g., JSON format)
+
+  training_history_file_path =simulation_path+'_training_history'
+  # training_history_file_path  =simulation_directory_path+training_history_file_name
+
+  training_history_file_path_json=training_history_file_path+'.json'
+  with open(training_history_file_path_json, 'w') as f:
+      json.dump(history.history, f)
+  print(training_history_file_path_json)
+
+  training_history_file_path_csv=training_history_file_path+'.csv'
+  pd.DataFrame.from_dict(history.history).to_csv(training_history_file_path_csv,index=False)
+  print(training_history_file_path_csv)
+
+  training_history_file_path_npy=training_history_file_path+'.npy'
+  np.save(training_history_file_path_npy,history.history)
+  print(training_history_file_path_npy)
+  return training_history_file_path_json,training_history_file_path_csv,training_history_file_path_csv
+
+def plot_training_history(history,simulation_path):
+    # Plot training & validation accuracy values
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    # Set ticks on the epoch axis to display only integer values
+    plt.xticks(range(0, len(history.history['accuracy'])+1,5))
+
+    # Plot training & validation loss values
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # Set ticks on the epoch axis to display only integer values
+    plt.xticks(range(0, len(history.history['accuracy'])+1,5))
+
+    # Adjust layout and show the plot
+    plt.tight_layout()
+
+
+    # Save the plot with high resolution (300 dpi)
+    file_name='_accuracy_loss.png'
+    file_path=simulation_path+file_name
+    plt.savefig(file_path, dpi=300)
+    plt.show()
+    plt.close()
+    return file_path
+
+def evaluate_model(model, x_test, y_test):
+    """
+    Evaluate the performance of a trained model on test data.
+
+    Parameters:
+    - model: The trained deep neural model.
+    - x_test: Test data, it shall be in the cloud points format, each entry contains 1024x3 data.
+    - y_test: True labels.
+
+    Returns:
+    - accuracy: Accuracy of the model on the test data.
+    - confusion_matrix: Confusion matrix for the predictions.
+    """
+    # Assuming model is your trained deep neural model
+    y_pred = model.predict(x_test)
+    y_pred_class = np.argmax(y_pred, axis=1)  # Extracting the class with the highest probability
+
+    # Assuming y_true is a Nx2 array where each row contains the true class probabilities
+    y_true_class = np.argmax(y_test, axis=1)  # Extracting the class with the highest true probability
+
+    accuracy = accuracy_score(y_true_class, y_pred_class)
+    print(f'Accuracy: {accuracy}')
+
+    cm = confusion_matrix(y_true_class, y_pred_class)
+    print(f'Confusion Matrix: {cm}')
+
+    return accuracy, cm
+
+
 
 
 
