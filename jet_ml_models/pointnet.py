4793b642e538258e4ea789051920dfede6833218
Haydar Mehryar <haydarmehryar@gmail.com>
2024-05-13 18:26:28 -0400

    pointnet_alpha_s impl ready to run on colab resolves #309 @hmehryar

--- jet_ml_models/pointnet.py
+++ jet_ml_models/pointnet.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import ModelCheckpoint
 from time import time
 
@@ -171,26 +172,11 @@ def split_dataset(dataset_x_points, dataset_y, test_size=0.2, random_state=None)
     - x_train_points, x_test_points: The split 3D coordinates for training and testing.
     - y_train, y_test: The split target values for training and testing.
     """
-    # @h.mehryar: I think this was redundant, not sure why I impllemented it, there is
-    # no need to flatten the dataset_x for splitting it
-    # Flatten the input data to 2D
-    #flattened_dataset_x = dataset_x.reshape(dataset_x.shape[0], -1)
-    
-    # Split the dataset
-    # x_train, x_test, x_train_points, x_test_points, y_train, y_test = \
-    #     train_test_split(flattened_dataset_x, dataset_x_points, dataset_y, test_size=test_size, random_state=random_state)
     
     x_train_points, x_test_points, y_train, y_test = \
         train_test_split(dataset_x_points, dataset_y, test_size=test_size, random_state=random_state)
 
-    # @h.mehryar: I think this was redundant, not sure why I impllemented it
-    # # Reshape the input data back to 3D
-    # x_train = x_train.reshape(x_train.shape[0], dataset_x.shape[1], dataset_x.shape[2])
-    # x_test = x_test.reshape(x_test.shape[0], dataset_x.shape[1], dataset_x.shape[2])
-
     return x_train_points, x_test_points, y_train, y_test
-
-from tensorflow.keras.utils import to_categorical
 
 def preprocess_dataset(dataset_x, dataset_y,is_one_hot_encoded=True):
     print("Pre-processing")
@@ -233,54 +219,6 @@ def preprocess_dataset(dataset_x, dataset_y,is_one_hot_encoded=True):
 
 
 
-
-def train_model_with_callbacks(model, x_train, y_train, x_test, y_test, monitor="val_accuracy", best_model_file_path="best_model.h5", n_epochs=10):
-    """
-    Train a TensorFlow model with specified callbacks for checkpointing and early stopping.
-
-    Parameters:
-    - model (tf.keras.Model): The TensorFlow model to be trained.
-    - x_train (numpy.ndarray): The training data features.
-    - y_train (numpy.ndarray): The training data labels.
-    - x_test (numpy.ndarray): The test data features.
-    - y_test (numpy.ndarray): The test data labels.
-    - monitor (str): Quantity to be monitored for early stopping and checkpointing. Default is "val_accuracy".
-    - best_model_file_path (str): File path to save the best model checkpoint. Default is "best_model.h5".
-    - n_epochs (int): Number of epochs for training. Default is 10.
-
-    Returns:
-    - model (tf.keras.Model): The trained TensorFlow model.
-    - history (tf.keras.callbacks.History): A History object containing training/validation metrics.
-    - train_time (float): Time taken for training in minutes.
-    """
-
-    # Define callbacks
-    checkpoint_callback = ModelCheckpoint(
-        best_model_file_path,
-        monitor=monitor,
-        save_best_only=True,
-        mode="max" if monitor == "val_accuracy" else "min",
-        verbose=1
-    )
-    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
-        monitor=monitor,    # Quantity to be monitored
-        patience=3,         # Number of epochs with no improvement after which training will be stopped
-        verbose=1           # Verbosity mode. 1: print messages when triggered, 0: silent
-    )
-    callbacks = [checkpoint_callback, early_stopping_callback]
-
-    start = time()
-    # Train the model
-    history = model.fit(
-        x_train,
-        y_train,
-        epochs=n_epochs,
-        validation_data=(x_test, y_test),
-        callbacks=callbacks
-    )
-    train_time = (time()-start)/60.0
-    return model, history, train_time
-
 def save_training_history(history,simulation_path):
   # Save the training history to a file (e.g., JSON format)
 
@@ -301,18 +239,184 @@ def save_training_history(history,simulation_path):
   print(training_history_file_path_npy)
   return training_history_file_path_json,training_history_file_path_csv,training_history_file_path_csv
 
-def plot_training_history(history,simulation_path):
+
+
+from tensorflow import data as tf_data
+
+def create_tf_dataset(x_data, y_data):
+    """
+    Create a TensorFlow Dataset from input data.
+
+    Parameters:
+    - x_data (numpy.ndarray): The features data.
+    - y_data (numpy.ndarray): The labels data.
+
+    Returns:
+    - dataset (tf.data.Dataset): TensorFlow Dataset containing input data.
+    """
+    dataset = tf.data.Dataset.from_tensor_slices((x_data, y_data))
+    return dataset
+
+def augment(points, label):
+    # jitter points
+    points += keras.random.uniform(points.shape, -0.005, 0.005, dtype="float64")
+    # shuffle points
+    points = keras.random.shuffle(points)
+    return points, label
+
+def prepare_datasets(dataset, test_dataset, len_x_train, len_x_test, augment, train_size=0.8, batch_size=32):
+    """
+    Prepare training, validation  and test datasets for model training. It will shuffle all data and then take the validation proportion out.
+
+    Parameters:
+    - dataset (tf.data.Dataset): TensorFlow Dataset containing training data.
+    - test_dataset (tf.data.Dataset): TensorFlow Dataset containing test data.
+    - len_x_train (int): Length of the features data for training.
+    - len_x_test (int): Length of the features data for testing.
+    - augment (callable): Function to apply data augmentation.
+    - train_size (float): Proportion of the dataset to use for training. Default is 0.8.
+    - batch_size (int): Batch size for training and validation datasets. Default is 32.
+
+    Returns:
+    - train_dataset (tf.data.Dataset): TensorFlow Dataset for training.
+    - validation_dataset (tf.data.Dataset): TensorFlow Dataset for validation.
+    - test_dataset (tf.data.Dataset): TensorFlow Dataset for testing.
+    """
+    # Determine training dataset size
+    train_dataset_size = int(len(dataset) * train_size)
+    
+    # Shuffle and augment datasets
+    dataset = dataset.shuffle(len_x_train).map(augment)
+    test_dataset = test_dataset.shuffle(len_x_test).batch(batch_size)
+    
+    # Split dataset into training and validation sets
+    train_dataset = dataset.take(train_dataset_size).batch(batch_size)
+    validation_dataset = dataset.skip(train_dataset_size).batch(batch_size)
+    
+    return train_dataset, validation_dataset, test_dataset
+
+def train_model_with_callbacks(model, x_train=None, y_train=None, x_validation=None, y_validation=None, train_dataset=None, validation_dataset=None, monitor="val_accuracy", best_model_file_path="best_model.h5", n_epochs=10):
+    """
+    Train a TensorFlow model with specified callbacks for checkpointing and early stopping.
+
+    Parameters:
+    - model (tf.keras.Model): The TensorFlow model to be trained.
+    - x_train (numpy.ndarray): The training data features (optional if train_dataset is provided).
+    - y_train (numpy.ndarray): The training data labels (optional if train_dataset is provided).
+    - x_test (numpy.ndarray): The test data features (optional if validation_dataset is provided).
+    - y_test (numpy.ndarray): The test data labels (optional if validation_dataset is provided).
+    - train_dataset (tf.data.Dataset): TensorFlow Dataset for training (optional if x_train and y_train are provided).
+    - validation_dataset (tf.data.Dataset): TensorFlow Dataset for validation (optional if x_test and y_test are provided).
+    - monitor (str): Quantity to be monitored for early stopping and checkpointing. Default is "val_accuracy".
+    - best_model_file_path (str): File path to save the best model checkpoint. Default is "best_model.h5".
+    - n_epochs (int): Number of epochs for training. Default is 10.
+
+    Returns:
+    - model (tf.keras.Model): The trained TensorFlow model.
+    - history (tf.keras.callbacks.History): A History object containing training/validation metrics.
+    - train_time (float): Time taken for training in minutes.
+    """
+
+    if train_dataset is None or validation_dataset is None:
+        if x_train is None or y_train is None or x_validation is None or y_validation is None:
+            raise ValueError("Either provide train_dataset and validation_dataset or provide x_train, y_train, x_test, and y_test.")
+
+        # Create datasets from provided arrays
+        train_dataset = create_tf_dataset(x_train, y_train)
+        validation_dataset = create_tf_dataset(x_validation, y_validation)
+
+    # Define callbacks
+    checkpoint_callback = ModelCheckpoint(
+        best_model_file_path,
+        monitor=monitor,
+        save_best_only=True,
+        mode="max" if monitor == "val_accuracy" else "min",
+        verbose=1
+    )
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor=monitor,    # Quantity to be monitored
+        patience=3,         # Number of epochs with no improvement after which training will be stopped
+        verbose=1           # Verbosity mode. 1: print messages when triggered, 0: silent
+    )
+    callbacks = [checkpoint_callback, early_stopping_callback]
+
+    start = time()
+    # Train the model
+    history = model.fit(
+        train_dataset,
+        epochs=n_epochs,
+        validation_data=validation_dataset,
+        callbacks=callbacks
+    )
+    train_time = (time()-start)/60.0
+    return model, history, train_time
+
+
+def evaluate_model(model, x_test=None, y_test=None, test_dataset=None):
+    """
+    Evaluate the performance of a trained model on test data.
+
+    Parameters:
+    - model: The trained deep neural model.
+    - x_test: Test data features (optional if test_dataset is provided).
+    - y_test: True labels (optional if test_dataset is provided).
+    - test_dataset: TensorFlow Dataset containing test data (optional if x_test and y_test are provided).
+
+    Returns:
+    - accuracy: Accuracy of the model on the test data.
+    - confusion_matrix: Confusion matrix for the predictions.
+    """
+    if test_dataset is None:
+        if x_test is None or y_test is None:
+            raise ValueError("Either provide test_dataset or provide x_test and y_test.")
+
+        y_pred = model.predict(x_test)
+        y_pred_class = np.argmax(y_pred, axis=1)
+
+        y_true_class = np.argmax(y_test, axis=1)
+
+    else:
+        y_pred_probs = model.predict(test_dataset)
+        y_pred_class = np.argmax(y_pred_probs, axis=1)
+
+        y_true_class = np.concatenate([y.numpy() for _, y in test_dataset], axis=0)
+
+    accuracy = accuracy_score(y_true_class, y_pred_class)
+    print(f'Accuracy: {accuracy}')
+
+    cm = confusion_matrix(y_true_class, y_pred_class)
+    print(f'Confusion Matrix: {cm}')
+
+    return accuracy, cm
+
+def plot_training_history(history, simulation_path):
+    """
+    Plot training and validation accuracy and loss values and save the plot with high resolution.
+
+    Parameters:
+    - history (tf.keras.callbacks.History): History object containing training/validation metrics.
+    - simulation_path (str): Path to save the plot.
+
+    Returns:
+    - file_path (str): File path of the saved plot.
+    """
     # Plot training & validation accuracy values
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
+    if 'accuracy' in history.history:
+        plt.plot(history.history['accuracy'])
+    if 'sparse_categorical_accuracy' in history.history:
+        plt.plot(history.history['sparse_categorical_accuracy'])
+    if 'val_accuracy' in history.history:
+        plt.plot(history.history['val_accuracy'])
+    if 'val_sparse_categorical_accuracy' in history.history:
+        plt.plot(history.history['val_sparse_categorical_accuracy'])
     plt.title('Model Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
-    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.legend(['Train', 'Train (sparse)', 'Validation', 'Validation (sparse)'], loc='upper left')
     # Set ticks on the epoch axis to display only integer values
-    plt.xticks(range(0, len(history.history['accuracy'])+1,5))
+    plt.xticks(range(0, len(history.history.get('accuracy', []))+1, 5))
 
     # Plot training & validation loss values
     plt.subplot(1, 2, 2)
@@ -324,50 +428,40 @@ def plot_training_history(history,simulation_path):
     plt.legend(['Train', 'Validation'], loc='upper left')
 
     # Set ticks on the epoch axis to display only integer values
-    plt.xticks(range(0, len(history.history['accuracy'])+1,5))
+    plt.xticks(range(0, len(history.history.get('accuracy', []))+1, 5))
 
     # Adjust layout and show the plot
     plt.tight_layout()
 
-
     # Save the plot with high resolution (300 dpi)
-    file_name='_accuracy_loss.png'
-    file_path=simulation_path+file_name
+    file_name = '_accuracy_loss.png'
+    file_path = simulation_path + file_name
     plt.savefig(file_path, dpi=300)
     plt.show()
     plt.close()
+
     return file_path
 
-def evaluate_model(model, x_test, y_test):
+
+def save_kfold_results(results_kfold, simulation_path):
     """
-    Evaluate the performance of a trained model on test data.
+    Save k-fold cross-validation results to a text file and display them in a table.
 
     Parameters:
-    - model: The trained deep neural model.
-    - x_test: Test data, it shall be in the cloud points format, each entry contains 1024x3 data.
-    - y_test: True labels.
+    - results_kfold (list of dict): List containing dictionaries of k-fold results.
+    - simulation_path (str): Path to save the results text file.
 
     Returns:
-    - accuracy: Accuracy of the model on the test data.
-    - confusion_matrix: Confusion matrix for the predictions.
+    - df_results_kfold (pd.DataFrame): DataFrame containing k-fold results.
     """
-    # Assuming model is your trained deep neural model
-    y_pred = model.predict(x_test)
-    y_pred_class = np.argmax(y_pred, axis=1)  # Extracting the class with the highest probability
-
-    # Assuming y_true is a Nx2 array where each row contains the true class probabilities
-    y_true_class = np.argmax(y_test, axis=1)  # Extracting the class with the highest true probability
-
-    accuracy = accuracy_score(y_true_class, y_pred_class)
-    print(f'Accuracy: {accuracy}')
-
-    cm = confusion_matrix(y_true_class, y_pred_class)
-    print(f'Confusion Matrix: {cm}')
-
-    return accuracy, cm
-
-
-
-
-
-
+    # Create a DataFrame from k-fold results
+    df_results_kfold = pd.DataFrame(results_kfold)
+    
+    # Save the DataFrame to a text file
+    results_kfold_path = simulation_path + '_results_kfold.txt'
+    df_results_kfold.to_csv(results_kfold_path, index=False, sep='\t')
+    
+    # Display results in a table
+    print(df_results_kfold)
+    
+    return df_results_kfold
