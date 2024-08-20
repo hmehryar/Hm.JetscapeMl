ae94058a6a54f503fa0f658eca6e1a2ce3fcfa38
Haydar Mehryar <haydarmehryar@gmail.com>
2024-08-20 07:55:34 -0400

    summarizing and cleaning the code. ready for validation methods implementation resolves #327,#325  @hmehryar

--- jet_ml/config.py
+++ jet_ml/config.py
@@ -27,3 +27,21 @@ try:
     
 except ModuleNotFoundError:
     pass 
+
+def check_environments_versions():
+    # What version of Python do you have?
+    import sys
+
+    import tensorflow.keras
+    import pandas as pd
+    import sklearn as sk
+    import tensorflow as tf
+
+    print(f"Tensor Flow Version: {tf.__version__}")
+    print(f"Keras Version: {tensorflow.keras.__version__}")
+    print()
+    print(f"Python {sys.version}")
+    print(f"Pandas {pd.__version__}")
+    print(f"Scikit-Learn {sk.__version__}")
+    gpu = len(tf.config.list_physical_devices('GPU'))>0
+    print("GPU is", "available" if gpu else "NOT AVAILABLE")
