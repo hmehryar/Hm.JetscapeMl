2797d757299f18162357d473bc6d95a289799472
Haydar Mehryar <haydarmehryar@gmail.com>
2024-11-12 15:01:05 -0500

    impl grid_runner, simplied the process due to getting way too complicated resolves #318 @hmehryar

--- jet_ml/grid_runner.sh
+++ jet_ml/grid_runner.sh
@@ -4,18 +4,15 @@
 # email: hmehryar@wayne.edu
 
 # Job name
-#SBATCH --job-name=alphas-pointnet-1000k
+#SBATCH --job-name=alphas-point-1000k-b128-nolr
 
 # Submit to the GPU QoS
-##SBATCH -q primary
 ##SBATCH -q gpu
 
 #SBATCH -q express
 #SBATCH -p ecscp
 
 # Request the GPU type
-##SBATCH --gres=gpu:tesla
-##SBATCH --gres=gpu:geforc
 ##SBATCH --gres=gpu:2
 #SBATCH --gres=gpu:nvidia_a100_80gb_pcie_1g.10gb:1
 
@@ -23,15 +20,16 @@
 ## SBATCH --constraint=v100
 
 # Total number of cores, in this example it will 1 node with 1 core each.
+##SBATCH -n 2
+##SBATCH -c 24
+
 #SBATCH -n 1
 #SBATCH -c 8
-
 
 ##SBATCH -N 1
 
 # Request memory
 ##SBATCH --mem=256G
-##SBATCH --mem-per-cpu=32
 #SBATCH --mem=150G
 
 # Mail when the job begins, ends, fails, requeues
@@ -41,19 +39,15 @@
 #SBATCH --mail-user=gy4065@wayne.edu
 
 # Set maximum time limit
-#SBATCH -t 100:0:0
+#SBATCH -t 500:0:0
 
-
-
-#
+# Set the output and error log based on the simulation name
 # Create an output file
 #SBATCH -o ../runner_scripts/alpha_s_pointnet_1000K_batch_size_128_wsu_grid_a100_cpu_8_mem_150gb_output_%j.out
 
 # Create an error file
 #SBATCH -e ../runner_scripts/alpha_s_pointnet_1000k_batch_size_128_wsu_grid_a100_cpu_8_mem_150gb_error_%j.err
 
-# List assigned GPU:
-#echo Assigned GPU: $CUDA_VISIBLE_DEVICES
 
 # Check state of GPU:
 nvidia-smi
@@ -65,14 +59,17 @@ ROOT_PATH="jet_ml/"
 # Define the file name as a variable
 NOTEBOOK_PATH="classifiers/alpha_s/"
 # NOTEBOOK_PATH="notebooks/"
+
 FILE_NAME="alpha_s_pointnet"
 # FILE_NAME="building_alpha_s_dataset_with_constant_q0"
+
 SERVER_NAME="wsu_grid_a100_cpu_8_mem_150gb"
 # SERVER_NAME="wsu_grid_v100_cpu_24_mem_256gb"
+
 # DATASET_SIZE="q0_2.5_1800k_batch_size_128"
 # DATASET_SIZE="q0_2.5_250_batch_size_128"
-DATASET_SIZE="1000k_batch_size_128"
-# DATASET_SIZE="1k"
+DATASET_SIZE="1000k_batch_size_128_no_lr"
+
 JOB_NAME="${FILE_NAME}_${DATASET_SIZE}_${SERVER_NAME}"
 OUTPUT_FILE="${JOB_NAME}_output_%j.out"
 ERROR_FILE="${JOB_NAME}_error_%j.err"
@@ -86,11 +83,9 @@ echo "Running the following notebook"
 echo $NOTEBOOK
 echo "##########################################"
 RUNNER_SCRIPTS_PATH="runner_scripts/"
-# PYTHON_SCRIPT="${RUNNER_SCRIPTS_PATH}${FILE_NAME}.py"
 PYTHON_SCRIPT="${RUNNER_SCRIPTS_PATH}${FILE_NAME}_${DATASET_SIZE}_${SERVER_NAME}.py"
 PYTHON_OUTPUT="${RUNNER_SCRIPTS_PATH}${FILE_NAME}_${DATASET_SIZE}_${SERVER_NAME}.output"
 
-# PYTHON_SCRIPT="${FILE_NAME}.py"
 echo "Running the following python script"
 echo $PYTHON_SCRIPT
 echo "##########################################"
@@ -98,14 +93,12 @@ echo "##########################################"
 
 
 # Converting Jupyter notebook to python script
-# cd /wsu/home/gy/gy40/gy4065/hm_jetscapeml_source/jet_ml/classifiers/alpha_s
 echo "Converting notebook to script"
-# 
 jupyter nbconvert --to python ${NOTEBOOK} --output ../../../${PYTHON_SCRIPT}
 # jupyter nbconvert --to python ${NOTEBOOK} --output ../../${PYTHON_SCRIPT}
 
 # Setting up python version and conda shell
-# echo "Setting up python version and conda shell and environment on Grid"
+echo "Setting up python version and conda shell and environment on Grid"
 ml python/3.7
 source /wsu/el7/pre-compiled/python/3.7/etc/profile.d/conda.sh
 conda init
@@ -115,7 +108,5 @@ conda activate tensorflow-gpu-v2.8
 # conda init
 # conda activate tensorflow
 
-
-# Running simulation
 echo "Running simulation"
 python -u ../${PYTHON_SCRIPT} | tee ../${PYTHON_OUTPUT}
