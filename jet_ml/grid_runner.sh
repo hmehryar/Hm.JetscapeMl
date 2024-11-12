bb878a2ef0a71c86158075669a2cccd904ecd8e0
Haydar Mehryar <haydarmehryar@gmail.com>
2024-11-12 15:33:06 -0500

    impl alpha_s pointnet no rl resolves #321 @hmehryar

--- jet_ml/grid_runner.sh
+++ jet_ml/grid_runner.sh
@@ -7,30 +7,30 @@
 #SBATCH --job-name=alphas-point-1000k-b128-nolr
 
 # Submit to the GPU QoS
-##SBATCH -q gpu
+#SBATCH -q gpu
 
-#SBATCH -q express
-#SBATCH -p ecscp
+##SBATCH -q express
+##SBATCH -p ecscp
 
 # Request the GPU type
-##SBATCH --gres=gpu:2
-#SBATCH --gres=gpu:nvidia_a100_80gb_pcie_1g.10gb:1
+#SBATCH --gres=gpu:2
+##SBATCH --gres=gpu:nvidia_a100_80gb_pcie_1g.10gb:1
 
 # Request v100 gpu
-## SBATCH --constraint=v100
+# SBATCH --constraint=v100
 
 # Total number of cores, in this example it will 1 node with 1 core each.
-##SBATCH -n 2
-##SBATCH -c 24
+#SBATCH -n 2
+#SBATCH -c 12
 
-#SBATCH -n 1
-#SBATCH -c 8
+##SBATCH -n 1
+##SBATCH -c 8
 
 ##SBATCH -N 1
 
 # Request memory
-##SBATCH --mem=256G
-#SBATCH --mem=150G
+#SBATCH --mem=256G
+##SBATCH --mem=150G
 
 # Mail when the job begins, ends, fails, requeues
 #SBATCH --mail-type=ALL
@@ -43,10 +43,10 @@
 
 # Set the output and error log based on the simulation name
 # Create an output file
-#SBATCH -o ../runner_scripts/alpha_s_pointnet_1000K_batch_size_128_wsu_grid_a100_cpu_8_mem_150gb_output_%j.out
+#SBATCH -o ../runner_scripts/alpha_s_pointnet_1000K_batch_size_128_no_lr_wsu_grid_v100_cpu_24_mem_256gb_output_%j.out
 
 # Create an error file
-#SBATCH -e ../runner_scripts/alpha_s_pointnet_1000k_batch_size_128_wsu_grid_a100_cpu_8_mem_150gb_error_%j.err
+#SBATCH -e ../runner_scripts/alpha_s_pointnet_1000k_batch_size_128_no_lr_wsu_grid_v100_cpu_24_mem_256gb_error_%j.err
 
 
 # Check state of GPU:
@@ -63,12 +63,12 @@ NOTEBOOK_PATH="classifiers/alpha_s/"
 FILE_NAME="alpha_s_pointnet"
 # FILE_NAME="building_alpha_s_dataset_with_constant_q0"
 
-SERVER_NAME="wsu_grid_a100_cpu_8_mem_150gb"
-# SERVER_NAME="wsu_grid_v100_cpu_24_mem_256gb"
+# SERVER_NAME="wsu_grid_a100_cpu_8_mem_150gb"
+SERVER_NAME="wsu_grid_v100_cpu_24_mem_256gb"
 
 # DATASET_SIZE="q0_2.5_1800k_batch_size_128"
 # DATASET_SIZE="q0_2.5_250_batch_size_128"
-DATASET_SIZE="1000k_batch_size_128_no_lr"
+DATASET_SIZE="1000k_batch_size_128_no_rl"
 
 JOB_NAME="${FILE_NAME}_${DATASET_SIZE}_${SERVER_NAME}"
 OUTPUT_FILE="${JOB_NAME}_output_%j.out"
